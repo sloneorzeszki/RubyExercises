f8b0f2f24fc5f1d0a96ac6314a838339030e027e
sloneorzeszki <13190250+sloneorzeszki@users.noreply.github.com>
2015-11-06 19:58:15 +0100

    caesar cipher & stock picker

---
+++ stock_picker.rb
@@ -0,0 +1,17 @@
+def stock_picker(prices)
+	max_profit = 0
+	days = []
+	prices.each_with_index do |x,y|
+		for i in y+1...prices.count
+			if prices[i] - x > max_profit
+				max_profit = prices[i] - x
+				days = [y,i].to_s
+			end	
+		end
+	end
+	puts days
+end
+
+stock_picker([17,3,6,9,15,8,6,1,10])
+
+
