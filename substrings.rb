77db2053ae34771e900122daa38224f2fc272639
sloneorzeszki <13190250+sloneorzeszki@users.noreply.github.com>
2015-11-06 23:25:09 +0100

    substring

---
+++ substrings.rb
@@ -0,0 +1,14 @@
+def substrings(str, arr)
+	results = Hash.new
+	arr.each do |word|
+		count = str.downcase.scan(/#{word.downcase}/).size
+		if count > 0
+			results[word] = count
+		end
+	end
+	puts results
+end
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+substrings("Howdy partner, sit down! How's it going?", dictionary)
