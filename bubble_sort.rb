719772f6352f528dcccc7a20d8e2f604bdea0d1e
sloneorzeszki <13190250+sloneorzeszki@users.noreply.github.com>
2015-11-10 21:55:43 +0100

    bubble sort final

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -3,14 +3,14 @@ def bubble_sort(arr)
 	while changed
 		changed = false
 		for i in 0..arr.size-2
-			if arr[i] > arr[i+1]
+			if arr[i]>arr[i+1]
 				arr[i],arr[i+1] = arr[i+1],arr[i] 
 				changed = true
 			end
 		end
 	end
 	
-	puts arr
+	puts arr.inspect
 end
  
 bubble_sort([4,3,78,2,0,2])
@@ -20,12 +20,17 @@ def bubble_sort_by(arr)
 	while changed
 		changed = false
 		for i in 0..arr.size-2
-			if arr[i] > arr[i+1]
+			if yield(arr[i],arr[i+1]) > 0
 				arr[i],arr[i+1] = arr[i+1],arr[i] 
 				changed = true
 			end
 		end
 	end
 	
-	puts arr
+	puts arr.inspect
+end
+
+
+bubble_sort_by(["hi","hello","hey"]) do |left,right|
+	left.length - right.length
 end
