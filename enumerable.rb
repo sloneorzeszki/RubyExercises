4870b41a2246beedf4e0741baa27e5c397f470c0
sloneorzeszki <13190250+sloneorzeszki@users.noreply.github.com>
2015-11-11 13:55:52 +0100

    my_count finished

--- enumerable.rb
+++ enumerable.rb
@@ -47,8 +47,28 @@ module Enumerable
 		true
 	end	
 	
+	def my_count(*arg)
+		count = 0
+		if arg[0] == nil
+			if block_given?
+				for i in self
+					count += 1 if yield i
+				end
+			else
+				for i in self
+					count += 1
+				end			
+			end
+		else
+			for i in self
+				count += 1 if i == arg[0]
+			end	
+		end	
+		count	
+	end
+	
 end
-
-puts %w[ant bear cat].my_all? { |word| word.length < 3 }
-puts %w[ant bear cat].my_all? { |word| word.length >= 4 } 
-puts [true, true, true].my_all?                              
+ary = [1, 2, 4, 2]
+puts ary.my_count               
+puts ary.my_count(2)            
+puts ary.my_count{ |x| x%2==0 }
