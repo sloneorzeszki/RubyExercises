56efa323f2304cadbed30748d22e6db50bafb936
sloneorzeszki <13190250+sloneorzeszki@users.noreply.github.com>
2015-11-17 09:59:18 +0100

    styling

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,15 +1,15 @@
 def stock_picker(prices)
-	max_profit = 0
-	days = []
-	prices.each_with_index do |x,y|
-		for i in y+1...prices.count
-			if prices[i] - x > max_profit
-				max_profit = prices[i] - x
-				days = [y,i].to_s
-			end	
-		end
-	end
-	puts days
+  max_profit = 0
+  days = []
+  prices.each_with_index do |x,y|
+    for i in y+1...prices.count
+      if prices[i] - x > max_profit
+        max_profit = prices[i] - x
+        days = [y,i].to_s
+      end  
+    end
+  end
+  puts days
 end
 
 stock_picker([17,3,6,9,15,8,6,1,10])
