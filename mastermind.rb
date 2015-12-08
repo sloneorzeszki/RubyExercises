946464d60a5e79204b27161edfc611116070e6e9
sloneorzeszki <13190250+sloneorzeszki@users.noreply.github.com>
2015-12-08 23:58:08 +0100

    comparing

--- mastermind.rb
+++ mastermind.rb
@@ -4,41 +4,67 @@ class Game
   def initialize
     @maker = Codemaker.new
     @breaker = Codebreaker.new
+    puts "There are four colors being randomly selected next to each other.",
+    "You goal is to guess what colors were drawn in what order. You have 10 tries.",
+    "Possible colors are yellow, red, green, cyan, blue, magenta.",  
+    "Please enter you combination with one blank space between each color:"
   end
+
 end
 
 class Codemaker
+  def initialize
+    @secret_pattern = []
+    @tries = 0
+  end
+  
   def generate_secret_pattern
     colors = ["yellow","red","green", "cyan", "blue", "magenta"]  
-    secret_pattern = [colors[rand(6)],colors[rand(6)],colors[rand(6)],colors[rand(6)]]
+    @secret_pattern = [colors[rand(6)],colors[rand(6)],colors[rand(6)],colors[rand(6)]]
   end
 
   def compare_patterns(guess_pattern)
     feedback = []
-    
-    guess_pattern.each_with_index do |peg,index|
-      if peg == secret_pattern[index] 
+    temp_pattern = @secret_pattern.dup
+
+    for i in 0..guess_pattern.size - 1
+      if guess_pattern[i] == temp_pattern[i]
         feedback << "red"
-      elsif secret_pattern.any?{|x| x == peg}
-        feedback << "white"
+        guess_pattern[i] = nil
+        temp_pattern[i] = nil
       end
     end
-    puts feedback
-  end
 
+    for i in 0..guess_pattern.size - 1
+      if guess_pattern[i] != nil && temp_pattern.any? {|x| x == guess_pattern[i]}
+        feedback << "white"
+        temp_pattern[temp_pattern.index(guess_pattern[i])] = nil
+        guess_pattern[i] = nil
+      end
+    end
+
+    if feedback.count("red") == 4
+      puts "YOU WON OH MY GOD OH-MY-GOD!!!"
+      exit
+    elsif
+      puts "Feedback: " + feedback.count("red").to_s + "x red and " + feedback.count("white").to_s + "x white. Try again:"
+    end
+  end
 end
 
 
 class Codebreaker
   def make_a_guess
     guess_pattern = gets.chomp.split()
-    
   end
 end
 
+tries = 0
 game = Game.new
-game = Codemaker.new
-code.generate_secret_pattern
+game.maker.generate_secret_pattern
 
-code.compare_patterns
-
+while tries < 10
+  guess = game.breaker.make_a_guess
+  game.maker.compare_patterns(guess)
+  tries =+ 1
+end
