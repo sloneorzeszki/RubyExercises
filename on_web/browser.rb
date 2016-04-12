fc5077f2466fa4497cdca8ab29e8d9869c3cbf91
sloneorzeszki <13190250+sloneorzeszki@users.noreply.github.com>
2016-04-12 23:28:15 +0200

    browser split to methods

--- on_web/browser.rb
+++ on_web/browser.rb
@@ -1,41 +1,50 @@
 require 'json'
 require 'socket'
 
-host = 'localhost'
-port = 2000
-path = "/index.html"
 
-puts "What do you want to do? [G]ET/[P]OST?"
-user_decision = gets.chomp
+class Browser
+	def initialize(host,port,path)
+		@host = host
+		@port = port
+		@path = path
+	end
 
-if user_decision.upcase == "G"
-	request = "GET #{path} HTTP/1.0\r\n\r\n"
-elsif user_decision.upcase == "P"
-	puts "Give us some more details."
-	puts "What is the name of the viking?"
-	viking_name = gets.chomp
-	puts "What is his email?"
-	viking_email = gets.chomp
-	new_user = {viking: {name: viking_name, email: viking_email}}.to_json
-	request = %q(
-		POST /path/script.cgi HTTP/1.0
-		From: thiscomputer@mail.com
-		User-Agent: HTTPTool/1.0
-		Content-Type: application/x-www-form-urlencoded
-		)
-	request << "Content-Length:" + "#{new_user}".size.to_s
-	request << #{new_user}
+	def make_connection
+		request = ask_for_action
+		viking_hash = request.slice(request.index("\r\n\r\n")+4..-1) #cut out the hash
+		socket = TCPSocket.open(@host, @port)
+		socket.puts(request)
+		response = socket.read
+		puts response
+	end
 
-else 
-	puts "I don't understand. Exiting."
-	exit
+	def ask_for_action
+		puts "What do you want to do? [G]ET/[P]OST?"
+		user_decision = gets.chomp
+
+		if user_decision.upcase == "G"
+			request = "GET #{@path} HTTP/1.0\r\n\r\n"
+		elsif user_decision.upcase == "P"
+			request = ask_for_post_data
+		else
+			puts "I don't understand. Exiting."
+			exit
+		end
+		return request
+	end
+
+	def ask_for_post_data
+		puts "What is the name of the viking?"
+		viking_name = gets.chomp
+		puts "What is his email?"
+		viking_email = gets.chomp
+		new_user = {viking: {name: viking_name, email: viking_email}}
+		request = "POST #{@path} HTTP/1.0\nContent-Length: " + "#{new_user}".size.to_s + "\r\n\r\n" + new_user.to_json
+	end
 end
 
-
-socket = TCPSocket.open(host, port)
-socket.print(request)
-response = socket.read
-puts response
+br = Browser.new('localhost',2000,'/index.html')
+br.make_connection
 
 #headers, body = response.split("\r\n\r\n", 2)
 #puts body
--- on_web/server.rb
+++ on_web/server.rb
@@ -5,19 +5,34 @@ server = TCPServer.open(2000)
 
 loop {
 	client = server.accept
-	request = client.gets
+	request = IO.readline(client.gets)
+	puts request
+	viking_hash = request.slice(request.index("\r\n\r\n")+4..-1) #cut out the hash
+	client.puts request.size
 	request_type = request.split[0]
+  puts request_type
 	if request_type == "GET"
-	  unless /\index.html/.match(request).nil?
-		index_contents = File.read('index.html') 
-		client.puts "HTTP/1.0 200 OK"
-		client.puts "Date: " + Time.now.ctime
-		client.puts "Content-Type: text/html"
-		client.puts "Size: " + index_contents.size.to_s
-		client.puts
-		client.puts index_contents
+		unless /\index.html/.match(request).nil?
+			index_contents = File.read('index.html')
+			client.puts "HTTP/1.0 200 OK"
+			client.puts "Date: " + Time.now.ctime
+			client.puts "Content-Type: text/html"
+			client.puts "Size: " + index_contents.size.to_s
+			client.puts
+			client.puts index_contents
+		else
+			client.puts "HTTP/1.0 404 Not Found"
+		end
+	elsif request_type == "POST"
+		new_user = /{.*}/.match(request)
+		params = {}
+		params = JSON.parse(new_user)
+		body = File.read('thanks.html')
+		body["<%= yield %>"] = params
+		client.puts body
 	else
-		client.puts "HTTP/1.0 404 Not Found"
+		puts "I don't know that request"
 	end
+
 	client.close
 }
