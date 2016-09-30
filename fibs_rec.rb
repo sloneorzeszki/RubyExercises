cfa030ae70c8ed366b53b61180952457b1eff31a
sloneorzeszki <13190250+sloneorzeszki@users.noreply.github.com>
2016-09-30 21:40:16 +0200

    merge_sort and merge

--- fibs_rec.rb
+++ fibs_rec.rb
@@ -1,7 +1,26 @@
-
-
-def fibs_rec(n, start = 0, add = 1)
-puts n == 1 ? add : fibs_rec(n - 1, add, start + add)
+def fibs(iterations)
+	x = 0
+	base = 0
+	add = 1
+	until x == iterations
+		result = base + add
+		add = base
+		base = result
+		puts result
+		x += 1
+	end
 end
 
-fibs_rec(5)
+#fibs(15)
+
+def fibs_rec(n, base=0, add=1)
+	if n == 1 
+	 	return 1 
+	else
+		fibs_rec(n - 1, base + add, base)
+	end
+end
+
+fibs_rec(4)
+
+
--- merge_sort.rb
+++ merge_sort.rb
@@ -1,31 +1,17 @@
-#podziel na pol
-#porownaj pierwszy element jednej polowy z pierwszym elementem drugiej polowy
-
-def merge(arr)
-puts arr.inspect.to_s + " size:" +arr.size.to_s
-
-	if arr.size > 1 
-		*[a] = merge(arr[0..(arr.size-1)/2])
-		*[b] = merge(arr[(arr.size-1)/2+1..-1])
-		
-		puts a
-		puts b
-		sorted=[]
-		
-		until a.empty? && b.empty?
-			x=a.shift || 0
-			y=b.shift || 0
-						
-			if x > y
-				sorted << y
-			else
-				sorted << x
-			end
-		end
-		
-	else
-		return arr
-	end
+def merge_sort(arr)
+	#puts arr.inspect.to_s + " size:" +arr.size.to_s
+	return arr if arr.size == 1 
+	a, b = merge_sort(arr[0..(arr.size-1)/2]), merge_sort(arr[(arr.size-1)/2+1..-1])
+	merge(a,b)
 end
 
-merge([15,4,78,50,8,23,42])
+def merge(a,b)
+	sorted = []
+	sorted.push(a[0] > b[0] ? b.shift : a.shift)
+	return sorted
+end
+
+
+
+
+puts merge_sort([15,4,78,50,8,23,42])
