439933d7966f034e058b75d3aaf277f3fbce19ae
sloneorzeszki <13190250+sloneorzeszki@users.noreply.github.com>
2016-09-30 21:58:00 +0200

    merge sort final

--- merge_sort.rb
+++ merge_sort.rb
@@ -2,16 +2,12 @@ def merge_sort(arr)
 	#puts arr.inspect.to_s + " size:" +arr.size.to_s
 	return arr if arr.size == 1 
 	a, b = merge_sort(arr[0..(arr.size-1)/2]), merge_sort(arr[(arr.size-1)/2+1..-1])
-	merge(a,b)
-end
 
-def merge(a,b)
 	sorted = []
-	sorted.push(a[0] > b[0] ? b.shift : a.shift)
-	return sorted
+	while a.size > 0 && b.size > 0
+		sorted.push(a[0] >= b[0] ? b.shift : a.shift)
+	end
+	sorted + a + b
 end
 
-
-
-
-puts merge_sort([15,4,78,50,8,23,42])
+p merge_sort([15,4,78,50,8,23,42])
