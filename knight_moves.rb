22c440f034f3292d2bcb5b3725447b7c613de9ac
sloneorzeszki <13190250+sloneorzeszki@users.noreply.github.com>
2016-10-02 17:46:53 +0200

    knight_moves shortened version

--- knight_moves.rb
+++ knight_moves.rb
@@ -1,7 +1,7 @@
 class Node
-	attr_accessor :val, :parent, :ch1, :ch2, :ch3, :ch4, :ch5, :ch6, :ch7, :ch8 
+	attr_accessor :val, :parent
 
-	def initialize(val, parent=nil, ch1=nil, ch2=nil, ch3=nil, ch4=nil, ch5=nil, ch6=nil, ch7=nil, ch8=nil)
+	def initialize(val, parent=nil)
 		@val = val
 		@parent = parent
 	end
@@ -17,31 +17,26 @@ class Knight
 		process
 	end
 
-	#generate all possible moves from the current position (in a form of child nodes for the current node)
+
+	#generate all possible moves from the current position - one way relation (child knows parent, parent doesn't know child - as the path goes bottom to top)
 	def possible_moves(root)
 		@current_node = root
 		x, y = root.val.first, root.val.last
-
-		@queue_nodes_temp << root.ch1=Node.new([x+2,y+1],root) if within_board?(x+2,y+1)
-		@queue_nodes_temp << root.ch2=Node.new([x+1,y+2],root) if within_board?(x+1,y+2)
-		@queue_nodes_temp << root.ch3=Node.new([x-1,y+2],root) if within_board?(x-1,y+2)
-		@queue_nodes_temp << root.ch4=Node.new([x-2,y+1],root) if within_board?(x-2,y+1)
-		@queue_nodes_temp << root.ch5=Node.new([x-2,y-1],root) if within_board?(x-2,y-1)
-		@queue_nodes_temp << root.ch6=Node.new([x-1,y-2],root) if within_board?(x-1,y-2)
-		@queue_nodes_temp << root.ch7=Node.new([x+1,y-2],root) if within_board?(x+1,y-2)
-		@queue_nodes_temp << root.ch8=Node.new([x+2,y-1],root) if within_board?(x+2,y-1) 
+		moves = [[x+2,y+1],[x+1,y+2],[x-1,y+2],[x-2,y+1],[x-2,y-1],[x-1,y-2],[x+1,y-2],[x+2,y-1]]
+		moves.each { |move| @queue_nodes_temp << Node.new(move,root) if within_board?(move)}
 		return @queue_nodes_temp
 	end
 
+
 	#check if the calculated move is within the chessboard range
-	def within_board?(x,y)
+	def within_board?(square)
 		squares_range = (0..7).to_a
-		return true if squares_range.include?(x) && squares_range.include?(y)
+		return true if squares_range.include?(square.first) && squares_range.include?(square.last)
 	end
+
 
 	#check if within the currently generated nodes/moves is one that is equal to target position
 	def check_for_solution(current_node, target_square)
-
 		loop do 
 			#@i+=1
 
@@ -79,4 +74,4 @@ class Knight
 	end
 end
 
-checking = Knight.new([0,4],[7,7])
+checking = Knight.new([0,4],[6,4])
